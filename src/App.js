f10e7052681d3f65e4af3a96c3eaa218c48ddd0f
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-02-25 00:12:25 +0500

    removed component import

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import Hello from "./Components/Hello";
-import React, {useEffect, useState} from "react";
+import React from "react";
 import axios from "axios";
 
 function App() {
 
-    const [catFact, setCatFact] = useState([]);
-    // API calling function
-    const callApi = async () => {
-
-        let api = `https://catfact.ninja/facts`;
-
-        try {
-            let response = await axios(
-                {
-                    method: "GET",
-                    url: api
-                }
-            );
-
-            if (response.status === 200) {
-                console.log(response.data.data);
-                let data = response.data.data;
-                setCatFact(data);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    useEffect(() => callApi(), []);
-
     return (
         <React.Fragment>
-            {
-                catFact.map(function (value, index) {
-                    return <Hello fact={value.fact} key={index}/>
-                })
-            }
+            <h1>Hello World</h1>
         </React.Fragment>
     );
 }
